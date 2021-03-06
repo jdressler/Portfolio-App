eca5de53d5fcdba12d149143dfe4b9a8a81fc8f2
Joshua Dressler <joshua.dressler@pointalliance.com>
2021-03-06 15:56:44 -0500

    small edits

--- src/components/MainPanel/MainPanel.js
+++ src/components/MainPanel/MainPanel.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import {Row, Col} from 'react-bootstrap'
+import React from "react";
+import { Row, Col } from "react-bootstrap";
 
 class MainPanel extends React.Component {
-    render(){
-        return(
-            <Row>
-                <Col md='1'></Col>
-                <Col md='3'>sdf</Col>
-                <Col md='1'></Col>
-                <Col md='2'></Col>
-                <Col md='1'></Col>
-                <Col md='3'></Col>
-                <Col md='1'></Col>
-            </Row>
-        )
-    }
+  render() {
+    return (
+      <Row>
+        <Col md="1"></Col>
+        <Col md="3">sdf</Col>
+        <Col md="1"></Col>
+        <Col md="2"></Col>
+        <Col md="1"></Col>
+        <Col md="3"></Col>
+        <Col md="1"></Col>
+      </Row>
+    );
+  }
 }
 
 export default MainPanel;
--- src/components/MyResume/MyResume.js
+++ src/components/MyResume/MyResume.js
@@ -1,26 +1,24 @@
-import React from 'react'
-import {Row, Col} from 'react-bootstrap'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faFileDownload} from '@fortawesome/free-solid-svg-icons'
-import './MyResume.css'
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFileDownload } from "@fortawesome/free-solid-svg-icons";
+import "./MyResume.css";
 
 class MyResume extends React.Component {
-    render(){
-        return(
-            <Row className='my-resume-row'>
-                <Col md='7'></Col>
-                <Col md='3' className='col-fix py-2' id='resume-label-container'>
-                    <h3 id='resume'>Download Resume</h3>
+  render() {
+    return (
+      <Row className="my-resume-row">
+        <Col md="7"></Col>
+        <Col md="3" className="col-fix py-2" id="resume-label-container">
+          <h3 id="resume">Download Resume</h3>
+        </Col>
 
-                </Col>
-               
-                <Col md='1' className='col-fix py-2'>
-                <FontAwesomeIcon id='download-resume' icon={faFileDownload}/>
-
-                </Col>
-            </Row>
-        )
-    }
+        <Col md="1" className="col-fix py-2">
+          <FontAwesomeIcon id="download-resume" icon={faFileDownload} />
+        </Col>
+      </Row>
+    );
+  }  
 }
 
 export default MyResume;
